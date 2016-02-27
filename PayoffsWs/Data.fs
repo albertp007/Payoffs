c3edc93d00928b5dfd0ea8ab1957d7a2aa9fe995
Albert Pang <albert.pang@me.com>
2016-02-27 14:52:58 +0800

    Moved to fsharp core 4.4.0.0 and added form for implied vol calculator

---
+++ PayoffsWs/Data.fs
@@ -0,0 +1,19 @@
+﻿namespace PayoffsWs
+
+module Data =
+
+  open Payoffs.Option
+
+  let OptionTypeList : (string * OptionType) list = [ "Call", Call; "Put", Put ]
+
+  type ImpliedVolQuery = {
+    s0: float
+    r: float
+    q: float
+    T: float
+    K: float
+    OptType: OptionType
+    price: float
+    precision: float
+    maxIteration: int
+  }
